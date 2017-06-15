7a6f2ffb3dd164c050d405a2e6f5033d1ee2a008
Abhi Agarwal <me@abhiagarwal.com>
2017-06-14 22:04:07 -0400

    Adding search parametrs

---
+++ search/common.go
@@ -0,0 +1,267 @@
+package search
+
+import (
+	elastic "github.com/news-ai/elastic-appengine"
+)
+
+var (
+	NewBaseURL = "https://search1.newsai.org"
+)
+
+type ElasticMGetQuery struct {
+	Ids []string `json:"ids"`
+}
+
+type ElasticCreatedByQuery struct {
+	Term struct {
+		CreatedBy int64 `json:"data.CreatedBy"`
+	} `json:"term"`
+}
+
+type ElasticSubjectQuery struct {
+	Term struct {
+		Subject string `json:"data.Subject"`
+	} `json:"term"`
+}
+
+type ElasticBaseSubjectQuery struct {
+	Term struct {
+		BaseSubject string `json:"data.BaseSubject"`
+	} `json:"term"`
+}
+
+type ElasticBaseOpenedQuery struct {
+	Term struct {
+		Opened int64 `json:"data.Opened"`
+	} `json:"term"`
+}
+
+type ElasticBaseClickedQuery struct {
+	Term struct {
+		Clicked int64 `json:"data.Clicked"`
+	} `json:"term"`
+}
+
+type ElasticIsSentQuery struct {
+	Term struct {
+		IsSent bool `json:"data.IsSent"`
+	} `json:"term"`
+}
+
+type ElasticCancelQuery struct {
+	Term struct {
+		Cancel bool `json:"data.Cancel"`
+	} `json:"term"`
+}
+
+type ElasticClientQuery struct {
+	Term struct {
+		Client string `json:"data.Client"`
+	} `json:"match"`
+}
+
+type ElasticTagQuery struct {
+	Term struct {
+		Tag string `json:"data.Tags"`
+	} `json:"match"`
+}
+
+type ElasticEmployersQuery struct {
+	Term struct {
+		Employers string `json:"data.Employers"`
+	} `json:"match"`
+}
+
+type ElasticAllQuery struct {
+	Term struct {
+		All string `json:"_all"`
+	} `json:"match"`
+}
+
+type ElasticArchivedQuery struct {
+	Term struct {
+		Archived bool `json:"data.Archived"`
+	} `json:"term"`
+}
+
+type ElasticListIdQuery struct {
+	Term struct {
+		ListId int64 `json:"data.ListId"`
+	} `json:"term"`
+}
+
+type ElasticEmailIdQuery struct {
+	Term struct {
+		EmailId int64 `json:"data.EmailId"`
+	} `json:"term"`
+}
+
+type ElasticUserIdQuery struct {
+	Term struct {
+		UserId int64 `json:"data.UserId"`
+	} `json:"term"`
+}
+
+type ElasticEmailToQuery struct {
+	Term struct {
+		To string `json:"data.To"`
+	} `json:"term"`
+}
+
+type ElasticContactIdQuery struct {
+	Term struct {
+		ContactId int64 `json:"data.ContactId"`
+	} `json:"term"`
+}
+
+type ElasticUsernameQuery struct {
+	Term struct {
+		Username string `json:"data.Username"`
+	} `json:"term"`
+}
+
+type ElasticInstagramUsernameQuery struct {
+	Term struct {
+		InstagramUsername string `json:"data.InstagramUsername"`
+	} `json:"term"`
+}
+
+type ElasticFeedUsernameQuery struct {
+	Term struct {
+		Type     string `json:"data.Type"`
+		Username string `json:"data.Username"`
+	} `json:"term"`
+}
+
+type ElasticUsernameMatchQuery struct {
+	Match struct {
+		Username string `json:"data.Username"`
+	} `json:"match"`
+}
+
+type ElasticPublicationIdQuery struct {
+	Term struct {
+		PublicationId int64 `json:"data.PublicationId"`
+	} `json:"term"`
+}
+
+type ElasticFeedUrlQuery struct {
+	Match struct {
+		FeedURL string `json:"data.FeedURL"`
+	} `json:"match"`
+}
+
+type ElasticBounceQuery struct {
+	Term struct {
+		BaseBounced bool `json:"data.Bounced"`
+	} `json:"term"`
+}
+
+type ElasticCreatedRangeQuery struct {
+	Range struct {
+		DataCreated struct {
+			From string `json:"from"`
+			To   string `json:"to"`
+		} `json:"data.Created"`
+	} `json:"range"`
+}
+
+type ElasticOpenedRangeQuery struct {
+	Range struct {
+		DataOpened struct {
+			GTE int64 `json:"gte"`
+		} `json:"data.Opened"`
+	} `json:"range"`
+}
+
+type ElasticClickedRangeQuery struct {
+	Range struct {
+		DataClicked struct {
+			GTE int64 `json:"gte"`
+		} `json:"data.Clicked"`
+	} `json:"range"`
+}
+
+type ElasticSortDataPublishDateQuery struct {
+	DataPublishDate struct {
+		Order string `json:"order"`
+		Mode  string `json:"mode"`
+	} `json:"data.PublishDate"`
+}
+
+type ElasticSortDataCreatedAtQuery struct {
+	DataCreatedAt struct {
+		Order string `json:"order"`
+		Mode  string `json:"mode"`
+	} `json:"data.CreatedAt"`
+}
+
+type ElasticSortDataCreatedQuery struct {
+	DataCreated struct {
+		Order string `json:"order"`
+		Mode  string `json:"mode"`
+	} `json:"data.Created"`
+}
+
+type ElasticSortDataQuery struct {
+	Date struct {
+		Order string `json:"order"`
+		Mode  string `json:"mode"`
+	} `json:"data.Date"`
+}
+
+func InitializeElasticSearch() {
+	tweetElastic := elastic.Elastic{}
+	tweetElastic.BaseURL = NewBaseURL
+	tweetElastic.Index = "tweets"
+	tweetElastic.Type = "tweet"
+	elasticTweet = &tweetElastic
+
+	twitterUserElastic := elastic.Elastic{}
+	twitterUserElastic.BaseURL = NewBaseURL
+	twitterUserElastic.Index = "tweets"
+	twitterUserElastic.Type = "user"
+	elasticTwitterUser = &twitterUserElastic
+
+	contactDatabaseElastic := elastic.Elastic{}
+	contactDatabaseElastic.BaseURL = NewBaseURL
+	contactDatabaseElastic.Index = "database"
+	contactDatabaseElastic.Type = "contacts"
+	elasticContactDatabase = &contactDatabaseElastic
+
+	headlineElastic := elastic.Elastic{}
+	headlineElastic.BaseURL = NewBaseURL
+	headlineElastic.Index = "headlines"
+	headlineElastic.Type = "headline"
+	elasticHeadline = &headlineElastic
+
+	feedElastic := elastic.Elastic{}
+	feedElastic.BaseURL = NewBaseURL
+	feedElastic.Index = "feeds"
+	feedElastic.Type = "feed"
+	elasticFeed = &feedElastic
+
+	instagramElastic := elastic.Elastic{}
+	instagramElastic.BaseURL = NewBaseURL
+	instagramElastic.Index = "instagrams"
+	instagramElastic.Type = "instagram"
+	elasticInstagram = &instagramElastic
+
+	instagramUserElastic := elastic.Elastic{}
+	instagramUserElastic.BaseURL = NewBaseURL
+	instagramUserElastic.Index = "instagrams"
+	instagramUserElastic.Type = "user"
+	elasticInstagramUser = &instagramUserElastic
+
+	instagramTimeseriesElastic := elastic.Elastic{}
+	instagramTimeseriesElastic.BaseURL = NewBaseURL
+	instagramTimeseriesElastic.Index = "timeseries"
+	instagramTimeseriesElastic.Type = "instagram"
+	elasticInstagramTimeseries = &instagramTimeseriesElastic
+
+	twitterTimeseriesElastic := elastic.Elastic{}
+	twitterTimeseriesElastic.BaseURL = NewBaseURL
+	twitterTimeseriesElastic.Index = "timeseries"
+	twitterTimeseriesElastic.Type = "twitter"
+	elasticTwitterTimeseries = &twitterTimeseriesElastic
+}
---
+++ search/database.go
@@ -0,0 +1,271 @@
+package search
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/context"
+
+	gcontext "github.com/gorilla/context"
+
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
+
+	pitchModels "github.com/news-ai/pitch/models"
+
+	elastic "github.com/news-ai/elastic-appengine"
+)
+
+var (
+	elasticContactDatabase *elastic.Elastic
+)
+
+type EnhanceResponse struct {
+	Data interface{} `json:"data"`
+}
+
+type EnhanceFullContactProfileResponse struct {
+	Data struct {
+		Status        int `json:"status"`
+		Organizations []struct {
+			StartDate string `json:"startDate,omitempty"`
+			EndDate   string `json:"endDate,omitempty"`
+			Name      string `json:"name,omitempty"`
+			Title     string `json:"title"`
+		} `json:"organizations"`
+		DigitalFootprint struct {
+			Topics []struct {
+				Value    string `json:"value"`
+				Provider string `json:"provider"`
+			} `json:"topics"`
+			Scores []struct {
+				Type     string `json:"type"`
+				Value    int    `json:"value"`
+				Provider string `json:"provider"`
+			} `json:"scores"`
+		} `json:"digitalFootprint"`
+		SocialProfiles []struct {
+			Username  string `json:"username,omitempty"`
+			Bio       string `json:"bio,omitempty"`
+			TypeID    string `json:"typeId"`
+			URL       string `json:"url"`
+			TypeName  string `json:"typeName"`
+			Type      string `json:"type"`
+			Followers int    `json:"followers,omitempty"`
+			ID        string `json:"id,omitempty"`
+			Following int    `json:"following,omitempty"`
+		} `json:"socialProfiles"`
+		Demographics struct {
+			LocationDeduced struct {
+				City struct {
+					Name string `json:"name"`
+				} `json:"city"`
+				Country struct {
+					Code    string `json:"code"`
+					Name    string `json:"name"`
+					Deduced bool   `json:"deduced"`
+				} `json:"country"`
+				DeducedLocation string `json:"deducedLocation"`
+				State           struct {
+					Code string `json:"code"`
+					Name string `json:"name"`
+				} `json:"state"`
+				NormalizedLocation string  `json:"normalizedLocation"`
+				Likelihood         float64 `json:"likelihood"`
+				Continent          struct {
+					Name    string `json:"name"`
+					Deduced bool   `json:"deduced"`
+				} `json:"continent"`
+			} `json:"locationDeduced"`
+			Gender          string `json:"gender"`
+			LocationGeneral string `json:"locationGeneral"`
+		} `json:"demographics"`
+		Photos []struct {
+			URL       string `json:"url"`
+			TypeID    string `json:"typeId"`
+			IsPrimary bool   `json:"isPrimary,omitempty"`
+			Type      string `json:"type"`
+			TypeName  string `json:"typeName"`
+		} `json:"photos"`
+		RequestID   string `json:"requestId"`
+		ContactInfo struct {
+			GivenName  string `json:"givenName"`
+			FullName   string `json:"fullName"`
+			FamilyName string `json:"familyName"`
+			Websites   []struct {
+				URL string `json:"url"`
+			} `json:"websites"`
+		} `json:"contactInfo"`
+		Likelihood float64 `json:"likelihood"`
+	} `json:"data"`
+}
+
+type EnhanceFullContactCompanyResponse struct {
+	Data struct {
+		Status    int    `json:"status"`
+		RequestID string `json:"requestId"`
+		Category  []struct {
+			Name string `json:"name"`
+			Code string `json:"code"`
+		} `json:"category"`
+		Logo           string `json:"logo"`
+		Website        string `json:"website"`
+		LanguageLocale string `json:"languageLocale"`
+		OnlineSince    string `json:"onlineSince"`
+		Organization   struct {
+			Name            string `json:"name"`
+			ApproxEmployees int    `json:"approxEmployees"`
+			Founded         string `json:"founded"`
+			ContactInfo     struct {
+				EmailAddresses []struct {
+					Value string `json:"value"`
+					Label string `json:"label"`
+				} `json:"emailAddresses"`
+				PhoneNumbers []struct {
+					Number string `json:"number"`
+					Label  string `json:"label"`
+				} `json:"phoneNumbers"`
+				Addresses []struct {
+					AddressLine1 string `json:"addressLine1"`
+					Locality     string `json:"locality"`
+					Region       struct {
+						Name string `json:"name"`
+						Code string `json:"code"`
+					} `json:"region"`
+					Country struct {
+						Name string `json:"name"`
+						Code string `json:"code"`
+					} `json:"country"`
+					PostalCode string `json:"postalCode"`
+					Label      string `json:"label"`
+				} `json:"addresses"`
+			} `json:"contactInfo"`
+			Links []struct {
+				URL   string `json:"url"`
+				Label string `json:"label"`
+			} `json:"links"`
+			Images []struct {
+				URL   string `json:"url"`
+				Label string `json:"label"`
+			} `json:"images"`
+			Keywords []string `json:"keywords"`
+		} `json:"organization"`
+		SocialProfiles []struct {
+			TypeID    string `json:"typeId"`
+			TypeName  string `json:"typeName"`
+			URL       string `json:"url"`
+			Bio       string `json:"bio,omitempty"`
+			Followers int    `json:"followers,omitempty"`
+			Following int    `json:"following,omitempty"`
+			Username  string `json:"username,omitempty"`
+			ID        string `json:"id,omitempty"`
+		} `json:"socialProfiles"`
+	} `json:"data"`
+}
+
+type DatabaseResponse struct {
+	Email string      `json:"email"`
+	Data  interface{} `json:"data"`
+}
+
+func searchESContactsDatabase(c context.Context, elasticQuery elastic.ElasticQuery) (interface{}, int, int, error) {
+	hits, err := elasticContactDatabase.QueryStruct(c, elasticQuery)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return nil, 0, 0, err
+	}
+
+	contactHits := hits.Hits
+	var contacts []interface{}
+	for i := 0; i < len(contactHits); i++ {
+		rawContact := contactHits[i].Source.Data
+		contactData := DatabaseResponse{
+			Email: contactHits[i].ID,
+			Data:  rawContact,
+		}
+		contacts = append(contacts, contactData)
+	}
+
+	return contacts, len(contactHits), hits.Total, nil
+}
+
+func SearchCompanyDatabase(c context.Context, r *http.Request, url string) (EnhanceFullContactCompanyResponse, error) {
+	contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
+	client := urlfetch.Client(contextWithTimeout)
+	getUrl := "https://enhance.newsai.org/company/" + url
+
+	req, _ := http.NewRequest("GET", getUrl, nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return EnhanceFullContactCompanyResponse{}, err
+	}
+
+	var enhanceResponse EnhanceFullContactCompanyResponse
+	err = json.NewDecoder(resp.Body).Decode(&enhanceResponse)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return EnhanceFullContactCompanyResponse{}, err
+	}
+
+	return enhanceResponse, nil
+}
+
+func SearchContactDatabase(c context.Context, r *http.Request, email string) (EnhanceFullContactProfileResponse, error) {
+	contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
+	client := urlfetch.Client(contextWithTimeout)
+	getUrl := "https://enhance.newsai.org/fullcontact/" + email
+
+	req, _ := http.NewRequest("GET", getUrl, nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return EnhanceFullContactProfileResponse{}, err
+	}
+
+	var enhanceResponse EnhanceFullContactProfileResponse
+	err = json.NewDecoder(resp.Body).Decode(&enhanceResponse)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return EnhanceFullContactProfileResponse{}, err
+	}
+
+	return enhanceResponse, nil
+}
+
+func SearchContactDatabaseForMediaDatabase(c context.Context, r *http.Request, email string) (pitchModels.MediaDatabaseProfile, error) {
+	contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
+	client := urlfetch.Client(contextWithTimeout)
+	getUrl := "https://enhance.newsai.org/fullcontact/" + email
+
+	req, _ := http.NewRequest("GET", getUrl, nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return pitchModels.MediaDatabaseProfile{}, err
+	}
+
+	var enhanceResponse pitchModels.MediaDatabaseProfile
+	err = json.NewDecoder(resp.Body).Decode(&enhanceResponse)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return pitchModels.MediaDatabaseProfile{}, err
+	}
+
+	return enhanceResponse, nil
+}
+
+func SearchESContactsDatabase(c context.Context, r *http.Request) (interface{}, int, int, error) {
+	offset := gcontext.Get(r, "offset").(int)
+	limit := gcontext.Get(r, "limit").(int)
+
+	elasticQuery := elastic.ElasticQuery{}
+	elasticQuery.Size = limit
+	elasticQuery.From = offset
+
+	return searchESContactsDatabase(c, elasticQuery)
+}
