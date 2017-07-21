6cae96c8e4eb1e173baf99444f4696737dcede5d
Abhi Agarwal <me@abhiagarwal.com>
2017-07-21 15:52:27 -0400

    small billing and closes #482

--- billing/start_subscription.go
+++ billing/start_subscription.go
@@ -47,42 +47,6 @@ func AddFreeTrialToUser(r *http.Request, user models.User, plan string) (int64, 
 	return billingId, nil
 }
 
-func CancelPlanOfUser(r *http.Request, user models.User, userBilling *models.Billing) error {
-	c := appengine.NewContext(r)
-	httpClient := urlfetch.Client(c)
-	sc := client.New(os.Getenv("STRIPE_SECRET_KEY"), stripe.NewBackends(httpClient))
-
-	if userBilling.IsOnTrial {
-		return errors.New("Can not cancel a trial")
-	}
-
-	customer, err := sc.Customers.Get(userBilling.StripeId, nil)
-	if err != nil {
-		var stripeError StripeError
-		err = json.Unmarshal([]byte(err.Error()), &stripeError)
-		if err != nil {
-			log.Errorf(c, "%v", err)
-			return errors.New("We had an error getting your user")
-		}
-
-		log.Errorf(c, "%v", err)
-		return errors.New(stripeError.Message)
-	}
-
-	// Cancel all plans they might have (they should only have one)
-	for i := 0; i < len(customer.Subs.Values); i++ {
-		sc.Subs.Cancel(customer.Subs.Values[i].ID, nil)
-	}
-
-	userBilling.IsCancel = true
-	userBilling.Save(c)
-
-	// Send an email to the user saying that the package will be canceled. Their account will be inactive on
-	// their "Expires" date.
-
-	return nil
-}
-
 func AddPlanToUser(r *http.Request, user models.User, userBilling *models.Billing, plan string, duration string, coupon string, originalPlan string) error {
 	c := appengine.NewContext(r)
 	httpClient := urlfetch.Client(c)
@@ -177,78 +141,6 @@ func AddPlanToUser(r *http.Request, user models.User, userBilling *models.Billin
 	err = emails.AddUserToTabulaePremiumList(c, user, originalPlan, emailDuration, ExpiresAt, billAmount, paidAmount)
 	if err != nil {
 		log.Errorf(c, "%v", err)
-	}
-
-	return nil
-}
-
-func SwitchUserPlanPreview(r *http.Request, user models.User, userBilling *models.Billing, duration, newPlan string) (int64, error) {
-	c := appengine.NewContext(r)
-	httpClient := urlfetch.Client(c)
-	sc := client.New(os.Getenv("STRIPE_SECRET_KEY"), stripe.NewBackends(httpClient))
-
-	customer, err := sc.Customers.Get(userBilling.StripeId, nil)
-	if err != nil {
-		var stripeError StripeError
-		err = json.Unmarshal([]byte(err.Error()), &stripeError)
-		if err != nil {
-			log.Errorf(c, "%v", err)
-			return 0.0, errors.New("We had an error getting your user")
-		}
-
-		log.Errorf(c, "%v", err)
-		return 0.0, errors.New(stripeError.Message)
-	}
-
-	if duration == "annually" {
-		newPlan = newPlan + "-yearly"
-	}
-
-	if customer.Subs.Count > 0 {
-		prorationDate := time.Now().Unix()
-
-		invoiceParams := &stripe.InvoiceParams{
-			Customer:         customer.ID,
-			Sub:              customer.Subs.Values[0].ID,
-			SubPlan:          newPlan,
-			SubProrationDate: prorationDate,
-		}
-		invoice, err := sc.Invoices.GetNext(invoiceParams)
-
-		if err != nil {
-			log.Errorf(c, "%v", err)
-			return 0.0, err
-		}
-
-		var cost int64 = 0
-		for _, invoiceItem := range invoice.Lines.Values {
-			if invoiceItem.Period.Start == prorationDate {
-				cost += invoiceItem.Amount
-			}
-		}
-
-		return cost, nil
-	}
-
-	return 0.00, nil
-}
-
-func SwitchUserPlan(r *http.Request, user models.User, userBilling *models.Billing, newPlan string) error {
-	c := appengine.NewContext(r)
-	httpClient := urlfetch.Client(c)
-	sc := client.New(os.Getenv("STRIPE_SECRET_KEY"), stripe.NewBackends(httpClient))
-
-	_, err := sc.Customers.Get(userBilling.StripeId, nil)
-	if err != nil {
-		var stripeError StripeError
-		err = json.Unmarshal([]byte(err.Error()), &stripeError)
-		if err != nil {
-			log.Errorf(c, "%v", err)
-			return errors.New("We had an error getting your user")
-		}
-
-		log.Errorf(c, "%v", err)
-		return errors.New(stripeError.Message)
 	}
 
 	return nil
