1ee9a3977f8fbc50f378f47068e6190abbd9c0fb
Abhi Agarwal <me@abhiagarwal.com>
2017-08-10 22:08:13 -0400

    consolidating models

--- search/database.go
+++ search/database.go
@@ -36,76 +36,16 @@ type EnhanceResponse struct {
 
 type EnhanceFullContactProfileResponse struct {
 	Data struct {
-		Status        int `json:"status"`
-		Organizations []struct {
-			StartDate string `json:"startDate,omitempty"`
-			EndDate   string `json:"endDate,omitempty"`
-			Name      string `json:"name,omitempty"`
-			Title     string `json:"title"`
-		} `json:"organizations"`
-		DigitalFootprint struct {
-			Topics []struct {
-				Value    string `json:"value"`
-				Provider string `json:"provider"`
-			} `json:"topics"`
-			Scores []struct {
-				Type     string `json:"type"`
-				Value    int    `json:"value"`
-				Provider string `json:"provider"`
-			} `json:"scores"`
-		} `json:"digitalFootprint"`
-		SocialProfiles []struct {
-			Username  string `json:"username,omitempty"`
-			Bio       string `json:"bio,omitempty"`
-			TypeID    string `json:"typeId"`
-			URL       string `json:"url"`
-			TypeName  string `json:"typeName"`
-			Type      string `json:"type"`
-			Followers int    `json:"followers,omitempty"`
-			ID        string `json:"id,omitempty"`
-			Following int    `json:"following,omitempty"`
-		} `json:"socialProfiles"`
-		Demographics struct {
-			LocationDeduced struct {
-				City struct {
-					Name string `json:"name"`
-				} `json:"city"`
-				Country struct {
-					Code    string `json:"code"`
-					Name    string `json:"name"`
-					Deduced bool   `json:"deduced"`
-				} `json:"country"`
-				DeducedLocation string `json:"deducedLocation"`
-				State           struct {
-					Code string `json:"code"`
-					Name string `json:"name"`
-				} `json:"state"`
-				NormalizedLocation string  `json:"normalizedLocation"`
-				Likelihood         float64 `json:"likelihood"`
-				Continent          struct {
-					Name    string `json:"name"`
-					Deduced bool   `json:"deduced"`
-				} `json:"continent"`
-			} `json:"locationDeduced"`
-			Gender          string `json:"gender"`
-			LocationGeneral string `json:"locationGeneral"`
-		} `json:"demographics"`
-		Photos []struct {
-			URL       string `json:"url"`
-			TypeID    string `json:"typeId"`
-			IsPrimary bool   `json:"isPrimary,omitempty"`
-			Type      string `json:"type"`
-			TypeName  string `json:"typeName"`
-		} `json:"photos"`
-		RequestID   string `json:"requestId"`
-		ContactInfo struct {
-			GivenName  string `json:"givenName"`
-			FullName   string `json:"fullName"`
-			FamilyName string `json:"familyName"`
-			Websites   []struct {
-				URL string `json:"url"`
-			} `json:"websites"`
-		} `json:"contactInfo"`
+		Status int `json:"status"`
+
+		Organizations    []pitchModels.Organization   `json:"organizations"`
+		DigitalFootprint pitchModels.DigitalFootprint `json:"digitalFootprint"`
+		SocialProfiles   []pitchModels.SocialProfile  `json:"socialProfiles"`
+		Demographics     pitchModels.Demographic      `json:"demographics"`
+		Photos           []pitchModels.Photo          `json:"photos"`
+		ContactInfo      pitchModels.ContactInfo      `json:"contactInfo"`
+
+		RequestID  string  `json:"requestId"`
 		Likelihood float64 `json:"likelihood"`
 	} `json:"data"`
 }
